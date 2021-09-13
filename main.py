dd2f9e65942ef3121e4b185c0c9eb3d4bc04fc6b
J.B Stepan <tonymoooon543@gmail.com>
2021-09-12 21:57:55 -0500

    Started the migration to Pycord
    Why am I moving away from Discord.py?
    Discord.py is of course no longer being maintained and so as time goes on it could start to break. I went with Pycord because it's a fork of Discord.py and is actively maintained.

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-import os
-import discord
-from discord.ext import commands
+import pycord
+from pycord.ext import commands
 
-import bot_settings as settings
-import web_dashboard.web_dash as webdash
+import config
 
-# Discord variables
-TOKEN = settings.token
-BOT = commands.Bot(command_prefix=settings.command_prefix)
-BOT.remove_command('help')
-rich_presence = f'Listening for {settings.command_prefix}'
+BOT = commands.Bot(command_prefix=config.PREFIX, description="A bot for me and my friends\' Discords.")
+COGS = []
 
-# Client events
 @BOT.event
-async def on_ready():
-    print('\n')
-    print('Bot as started')
-    #webdash.start_dash(BOT)
+async def on_connect():
+    print("\n")
+    print("Bot online!")
 
+# Loading cogs
+for i in range(len(COGS)):
+    COGS[i].setup(BOT)
 
-#############
-for filename in os.listdir('C:/Users/wayne/Desktop/Jakob2/JB Programs/JB Bot/cogs'):
-    if filename.endswith('.py'):
-        BOT.load_extension(f'cogs.{filename[:-3]}')
-        print('Loaded cog file ' + filename)
-
-# Run the bot!
-BOT.run(TOKEN)
+BOT.run(token=config.TOKEN)
--- web_dashboard/web_dash.py
+++
@@ -1,4 +0,0 @@
-import web_dashboard.webdash.app as app
-
-def start_dash(bot, port=5000):
-    app.start(bot, port)
