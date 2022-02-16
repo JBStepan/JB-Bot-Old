1ac8320772f9182a6db9c64e99454beb409de017
J.B Stepan <tonymoooon543@gmail.com>
2022-02-15 22:39:52 -0600

    Reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeework

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,66 +1,77 @@
-from unicodedata import name
 import discord
 from discord.ext import commands
 
-# Other import
+# # Other import
 import config
+import asyncio
 
 
 COG_NAME = 'Moderator'
 enabled = True
 
-# Cog variables
+# # Cog variables
+muted_people = []
 
-# Cog Class
+# # Cog Class
 class Moderator(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    # Events
+# Events
 
-    # Commands 
+# Commands 
     @commands.command(name='clear')
     async def _clear(self, ctx: commands.Context, amount: int=10):
         await ctx.message.delete()
         await ctx.channel.purge(limit=amount)
         await ctx.send(f"Cleared {amount} messages! <a:rave_parrot:886489050624692235>", delete_after=2)
 
+    @discord.slash_command(description="Clears the current channel of the given amount of messages", guild_ids=[883774092505940019])
+    async def __clear(self, ctx: discord.ApplicationContext, amount: int):
+        await ctx.message.delete()
+        await ctx.channel.purge(limit=amount)
+        await ctx.send(f"Cleared {amount} messages! <a:rave_parrot:886489050624692235>", delete_after=2)
+
     @commands.command(name="warn")
     async def _warn(self, ctx: commands.Context, member: discord.Member, *, reason: str=None):
-        await ctx.message.delete()
+         print(f"{ctx.message.author.display_name} has warned {member.display_name} for, {reason}")
+         await ctx.message.delete()
 
-        embed = discord.Embed(title="You have been warned!")
-        embed.color = discord.Color.red()
-        embed.add_field(name="Reason", value=reason)
-        embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar.url)
+         embed = discord.Embed(title="You have been warned!")
+         embed.color = discord.Color.red()
+         embed.add_field(name="Reason", value=reason)
+         embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar.url)
 
-        print("Ok")
-        await member.send(embed=embed)
+         await member.send(embed=embed)
     
     @commands.command(name="kick")
     async def _kick(self, ctx: commands.Context, member: discord.Member, *, reason: str=None):
+        print(f"{ctx.message.author.display_name} has kicked {member.display_name} for, {reason}")
         await ctx.message.delete()
         
-        
-
         embed = discord.Embed(title="You've been kicked!")
         embed.color = discord.Color.red()
         embed.add_field(name="Reason", value=reason)
         embed.description = "Since you've been kicked you can rejoin. But if you are kick 3 times that is a ban!"
         embed.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.display_avatar.url)
-
-        await member.send(embed=embed)
+        await member.send(embed=embed)         
         await member.kick()
 
-    # Helper Functions
+    @commands.command(name='mute')
+    async def _mute(self, ctx: commands.Context, user: discord.Member, reason):
+        print(f"{ctx.message.author.display_name} has muted {user.display_name} for, {reason}")
+        await ctx.message.delete()
 
-    # Any other function
+
+#     # Helper Functions
+
+#     # Any other function
 
 def setup(bot):
-    bot.add_cog(Moderator(bot))
+     bot.add_cog(Moderator(bot))
 
 def get_name():
-    return COG_NAME
+     return COG_NAME
 
 def help_embed():
     embed = discord.Embed(title="Template Pugin", description="""
